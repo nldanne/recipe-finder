e9ebbd61df8f92fa6dc9aa4dc75134295b71a5d6
nldanne <nldanne97@gmail.com>
2022-03-23 15:20:06 +0100

    imorted BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import Pages from './pages/Pages';
+import Pages from './pages/Pages.jsx';
+import Category from './components/Category.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+    <BrowserRouter>
+      <Category />
       <Pages />
+    </BrowserRouter>
     </div>
   );
 }
