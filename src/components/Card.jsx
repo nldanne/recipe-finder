1d8ec6d629e82cc6e650746d31530098acc87882
nldanne <nldanne97@gmail.com>
2022-03-23 13:51:59 +0100

    added gradient

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Card.css';
 
 function Card({ recipe }) {
   return (
     <div className='card'>
-      <p>{recipe.title}</p>
+      <p className='card-title'>{recipe.title}</p>
       <img src={recipe.image} alt={recipe.title} />
+      <div className='gradient-bg'></div>
     </div>
   )
 }
