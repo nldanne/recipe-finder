65ddd8bf993525d49b18b4fddebba08506ebc4ea
nldanne <nldanne97@gmail.com>
2022-03-23 15:20:38 +0100

    setup react-router-dom

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,11 +1,16 @@
-import Home from "./Home.jsx";
+import Home from './Home.jsx';
+import { Route, Routes } from 'react-router-dom';
+import Cuisine from './Cuisine.jsx';
 
 function Pages() {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cuisine/:type' element={<Cuisine />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default Pages;
