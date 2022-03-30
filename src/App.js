0530ec4a98749432c7a96ac32bff2bb164f53866
nldanne <nldanne97@gmail.com>
2022-03-30 15:04:31 +0200

    added logo navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import Pages from './pages/Pages.jsx';
 import Category from './components/Category.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
+import Search from './components/Search.jsx';
+import styled from 'styled-components';
+import { GiKnifeFork } from 'react-icons/gi';
 
 function App() {
   return (
     <div className="App">
     <BrowserRouter>
+    <Nav>
+      <Logo to={'/'} > DeliciousEats </Logo>
+      <GiKnifeFork />
+    </Nav>
+      <Search />
       <Category />
       <Pages />
     </BrowserRouter>
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-family: 'Lobster Two', cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  svg {
+    font-size: 2rem;
+  }
+`;
+
 
 export default App;
