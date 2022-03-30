938561ab013ea8308e03e6f76ef5e0ec621a40a4
nldanne <nldanne97@gmail.com>
2022-03-30 15:03:36 +0200

    updated routes

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,14 +1,22 @@
-import Home from './Home.jsx';
-import { Route, Routes } from 'react-router-dom';
-import Cuisine from './Cuisine.jsx';
+import Home from "./Home.jsx";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Cuisine from "./Cuisine.jsx";
+import Searched from "./Searched.jsx";
+import Recipe from "./Recipe.jsx";
+import { AnimatePresence } from "framer-motion";
 
 function Pages() {
+  const location = useLocation();
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/cuisine/:type' element={<Cuisine />} />
-      </Routes>
+      <AnimatePresence exitBeforeEnter>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/cuisine/:type" element={<Cuisine />} />
+          <Route path="/searched/:search" element={<Searched />} />
+          <Route path="/recipe/:name" element={<Recipe />} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
