7c211495d38b77a45b896dcee5caef30f55e8e15
nldanne <nldanne97@gmail.com>
2022-03-30 15:00:49 +0200

    added framer motion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import Vegetarian from "../components/Vegetarian.jsx";
 import Popular from "../components/Popular.jsx";
+import { motion } from "framer-motion";
 
 function Home() {
   return (
-    <div>
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.3 }}
+    >
       <Vegetarian />
       <Popular />
-    </div>
-  )
+    </motion.div>
+  );
 }
 
 export default Home;
